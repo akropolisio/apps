cc2a528817f89d85a56cb5c38c9692cefce026e8
Dmitriy Serdtsev <in19farkt@gmail.com>
2019-09-30 18:46:27 +0300

    Updated akro types

--- packages/react-api/src/akroTypes.ts
+++ packages/react-api/src/akroTypes.ts
@@ -38,11 +38,13 @@ export const akroTypes: RegistryTypes = {
   },
   Token: {
     token_id: 'u32',
+    decimals: 'u16',
     symbol: 'Vec<u8>'
   },
   Status: {
     _enum: [
       'Pending',
+      'Deposit',
       'Withdraw',
       'Approved',
       'Canceled',
@@ -54,7 +56,8 @@ export const akroTypes: RegistryTypes = {
     eth_address: 'H160',
     substrate_address: 'AccountId',
     amount: 'TokenBalance',
-    status: 'Status'
+    status: 'Status',
+    direction: 'Status'
   },
   BridgeTransfer: {
     transfer_id: 'ProposalId',
