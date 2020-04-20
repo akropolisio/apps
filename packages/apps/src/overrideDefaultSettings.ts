17f0f43dc5bfb1e8c61db6378513253052325bc0
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-04-20 17:02:28 +0300

    Fix overriding default node url

--- packages/apps/src/overrideDefaultSettings.ts
+++ packages/apps/src/overrideDefaultSettings.ts
@@ -1,11 +1,12 @@
 /* eslint-disable header/header */
 import uiSettings from '@polkadot/ui-settings';
+import { availableEndpoints } from '@polkadot/apps-config/settings';
 import { AKRO_NODES } from '@polkadot/apps-config/settings/endpoints';
 
 export function overrideDefaultSettings (): void {
   const settings = uiSettings.get();
 
-  const isDefaultNode = !AKRO_NODES.find(({ value }): boolean => {
+  const isDefaultNode = !availableEndpoints.find(({ value }): boolean => {
     return value === settings.apiUrl;
   });
 
